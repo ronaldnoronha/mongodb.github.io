28e6a930b59ea0dff244b16f533514756909d2b9
Ronald <ronald.noronha@sydney.edu.au>
2021-02-18 01:26:35 +1100

    updating files

--- delete_db.sh
+++ delete_db.sh
@@ -3,5 +3,5 @@
 mlaunch stop
 rm -rf data
 mlaunch init --replicaset --nodes 1 --sharded 3
-mongo config --eval "db.settings.save({_id:'chunksize', value:1})"
+mongo config --eval "db.settings.save({_id:'chunksize', value:32})"
 python create_cluster.py
